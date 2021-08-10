b301956384a8689f36d5c88be308693373a1a4c1
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-08-10 14:35:45 +0300

    Correct version of that

--- main.cpp
+++ main.cpp
@@ -3,67 +3,94 @@
 #include <typeinfo>
 using namespace std;
 
-template <typename T>
+union myUnion{
+    int i;
+    double d;
+    float f;
+    unsigned int j;
+    char c;
+    bool b;
+};
+
 class AnyType {
 private:
-    T value;
+    myUnion value;
+    string type;
     map<string ,string> types = {{"i","int"},{"j","unsigned int"},{"c","char"},{"f","float"},{"d","double"},{"b","bool"}};
+
+    template <typename T>
+    void assign(T Value){
+        if(typeid(Value).name()==typeid(AnyType).name()){
+            AnyType temp = Value;
+            this->value = temp.value;
+            this->type = temp.type;
+        }else
+            if(this->types.count(typeid(Value).name())==0){
+                throw "Cannot store that value in the object!";
+            }else{
+                this->type = typeid(Value).name();
+                if(this->type=="i"){
+                    this->value.i = Value;
+                }
+                if(this->type=="f"){
+                    this->value.f = Value;
+                }
+                if(this->type=="d"){
+                    this->value.d = Value;
+                }
+                if(this->type=="c"){
+                    this->value.c = Value;
+                }
+                if(this->type=="b"){
+                    this->value.b = Value;
+                }
+                if(this->type=="j"){
+                    this->value.j = Value;
+                }
+            }
+    }
 public:
     AnyType(){
-        this->value = 0;
+        this->type = "";
     }
+    template <typename T>
     AnyType(T value){
-        this->value = value;
+        assign(value);
     }
     AnyType(const AnyType& obj) {
         this->value = obj.value;
+        this->type = obj.type;
     }
     ~AnyType(){
 
     }
-    T getValue(){
-        return this->value;
-    }
     string getTypeName(){
-        return this->types[typeid(this->value).name()];
+        return types[this->type];
     }
     bool toBool() {
-        return bool(value);
+        return this->value.b;
     }
     int toInt() {
-        return int(value);
+        return this->value.i;
     }
     float toFloat() {
-        return float(value);
+        return this->value.f;
     }
     double toDouble() {
-        return double(value);
+        return this->value.d;
     }
     char toChar(){
-        return char(value);
+        return this->value.c;
     }
     unsigned int toUnsignedInt(){
-        return (unsigned int)(value);
+        return this->value.j;
     }
-    AnyType& operator+=(const AnyType& other){
-        this->value += other.value;
-        return *this;
-    }
-    AnyType& operator-=(const AnyType& other){
-        this->value /= other.value;
-        return *this;
-    }
-    AnyType& operator*=(const AnyType& other){
 
-        this->value /= other.value;
-        return *this;
+    template <typename T>
+    AnyType& operator=(T other){
+        assign(other);
     }
-    AnyType& operator/=(const AnyType& other){
-
-        this->value /= other.value;
-        return *this;
-    }
-    static void swap(T& a,T& b){
+    static void swap(AnyType& a,AnyType& b){
         AnyType t = a;
         a = b;
         b = t;
@@ -71,5 +98,12 @@ public:
 };
 
 int main() {
-
+    cout<<"Before\n";
+    AnyType a;
+    a = 1;
+    AnyType b;
+    b = true;
+    cout<<a.getTypeName()<<endl;
+    cout<<b.getTypeName()<<endl;
+    AnyType::swap(a,b);
 }
